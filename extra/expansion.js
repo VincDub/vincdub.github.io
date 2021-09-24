bce23cfdefc9f42196f96f72071e4e08af814823
VincDub <vincent.dubois089@gmail.com>
2021-09-24 23:24:03 +0200

    ajustement marges

--- extra/expansion.js
+++ extra/expansion.js
@@ -3,11 +3,14 @@ function expand(ckbx) {
     console.log(ctn_style.paddingBottom)
     var taille_bot="150px";
     var taille_top="50px";
+    var taille_rows="180px";
 
     if (ctn_style.paddingBottom == taille_bot) {
         taille_bot="10px";
         taille_top="10px";
+        taille_rows="100px";
     }
     ckbx.parentElement.style.paddingBottom = taille_bot;
     ckbx.parentElement.style.paddingTop = taille_top;
+    ckbx.parentElement.style.gridTemplateRows = taille_rows;
 }
